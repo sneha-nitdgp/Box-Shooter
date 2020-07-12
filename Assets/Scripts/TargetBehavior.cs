93fb4750d47dfc4f4ad16a9f9a3c0911a0eb44ca
Snehautp <snehautp@gmail.com>
2020-07-13 01:08:33 +0530

    Added Assets

---
+++ Assets/Scripts/TargetBehavior.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetBehavior : MonoBehaviour
+{
+
+	// target impact on game
+	public int scoreAmount = 0;
+	public float timeAmount = 0.0f;
+
+	// explosion when hit?
+	public GameObject explosionPrefab;
+
+	// when collided with another gameObject
+	void OnCollisionEnter (Collision newCollision)
+	{
+		// exit if there is a game manager and the game is over
+		if (GameManager.gm) {
+			if (GameManager.gm.gameIsOver)
+				return;
+		}
+
+		// do stuff if hit by a projectile
+		if (newCollision.gameObject.tag == "Projectile") {
+			if (explosionPrefab) {
+				// Instantiate an explosion effect at the gameObjects position and rotation
+				Instantiate (explosionPrefab, transform.position, transform.rotation);
+			}
+
+			// if game manager exists, make adjustments based on target properties
+			if (GameManager.gm) {
+				GameManager.gm.targetHit (scoreAmount, timeAmount);
+			}
+				
+			// destroy the projectile
+			Destroy (newCollision.gameObject);
+				
+			// destroy self
+			Destroy (gameObject);
+		}
+
+        // do stuff if hit by a rod
+        if (newCollision.gameObject.tag == "Rod")
+        {
+            if (explosionPrefab)
+            {
+                // Instantiate an explosion effect at the gameObjects position and rotation
+                Instantiate(explosionPrefab, transform.position, transform.rotation);
+            }
+
+            // destroy self
+            Destroy(gameObject);
+        }
+
+    }
+}
